2ffa81c9b0e2def3b3c42b2e82e88081aa74643a
lrvick <lance@lrvick.net>
2014-01-03 11:13:37 -0500

    First pass at making everything more pip-friendly

---
+++ README.md
@@ -0,0 +1,29 @@
+# Wearscript for Python #
+<http://github.com/OpenShades/wearscript>
+
+## About ##
+
+Various hooks and command line tools for interacting with wearscript in Python.
+
+
+## Requirements ##
+
+  * libevent-dev 
+  * pip
+  * python 2.* ( Dependant libraries don't support 3 yet )
+
+## Installation ##
+
+The easiest way to install with needed library dependencies is with pip
+
+### Stable ###
+
+    ``bash
+    pip install wearscript
+    ``
+
+### Development ###
+
+    ``bash
+    pip install -e git+https://github.com/OpenShades/wearscript-python/#egg=wearscript
+    ``
--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 from distutils.core import setup
 
-setup(name='wearscript',
-      version='0.0.1',
-      packages=['wearscript'],
-      scripts=['bin/wearscript'],
-      author='Brandyn A. White',
-      author_email='bwhite@dappervision.com',
-      license='Apache 2.0')
+setup(
+    name='wearscript',
+    version='0.1',
+    packages=['wearscript'],
+    scripts=['bin/wearscript'],
+    author='Brandyn A. White',
+    author_email='bwhite@dappervision.com',
+    license='Apache 2.0',
+    description='Python hooks and command line client for the WearScript \
+    Project',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'gevent',
+        'gevent-websocket',
+        'websocket-client',
+        'msgpack-python'
+    ]
+)
